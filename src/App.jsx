c94a80a3b8d69bd8bcef3e256402925793920a77
PedroPiveta <ppivetabarrotti208@gmail.com>
2023-04-17 15:54:23 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+function App() {
+
+  return (
+    <div className="App">
+      Bill
+
+      Select Tip %
+      5%
+      10%
+      15%
+      25%
+      50%
+      Custom
+        
+      Number of People
+
+      Tip Amount
+      / person
+
+      Total
+      / person
+
+      Reset
+
+    </div>
+  )
+}
+
+export default App
